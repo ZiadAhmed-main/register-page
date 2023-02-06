a73d6e1ebab587866c145c007223e1cc45e94437
z1adfps <ziadahmedcf@gmail.com>
2023-02-06 02:41:16 +0200

    mod

--- index/index.js
+++
@@ -1,9 +0,0 @@
-let img = document.querySelector('.imgsrc');
-let container = document.querySelector('.container');
-function phones(newsource) {
-    img.src = newsource;
-}
-
-function colors(color) {
-    container.style.background = color;
-}
