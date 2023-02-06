241b476bf2febc4c8b9186f0b787726be62bed10
Ziad Ahmed <110579421+ZiadAhmed-main@users.noreply.github.com>
2023-02-06 02:34:56 +0200

    add index file

---
+++ index/index.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.imgsrc');
+let container = document.querySelector('.container');
+function phones(newsource) {
+    img.src = newsource;
+}
+
+function colors(color) {
+    container.style.background = color;
+}
